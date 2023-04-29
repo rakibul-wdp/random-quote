2009063aee729d2fe2ecdaa94389ee139a656f31
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-29 09:56:03 +0600

    traverse array elements boilarplate

---
+++ traverseArrayElements.js
@@ -0,0 +1,24 @@
+/**
+ * @Title: Traverse Array Elements
+ */
+
+const arr = [1, 2, 3, 4, 5];
+
+/**
+ * 1 2 3 4 5
+ * 0 1 2 3 4
+ *
+ * "A" "B" "C" "C"
+ *  0   1   2   3
+ *
+ * 1 B 3 5 F O true
+ * 0 1 2 3 4 5 6
+ */
+
+// Simple Traverse
+
+
+// Array Sum & Avg
+
+
+// Find Large Number
