6e2b39b59886b8f863ba25837852fa873404f263
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-29 09:48:40 +0600

    factory pattern

--- constructorVsFactory.js
+++ constructorVsFactory.js
@@ -13,3 +13,14 @@ const a3 = new Array(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
 console.log(a3, a3.length);
 
 // Factory Pattern
+const b1 = Array(); // []
+console.log(b1, b1.length);
+
+const b2 = Array(5);
+console.log(b2, b2.length);
+
+const b3 = Array(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
+console.log(b3, b3.length);
+
+console.log(a1.__proto__.constructor);
+console.log(b1.__proto__.constructor);
