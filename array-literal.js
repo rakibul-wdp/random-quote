38756c528a479ab70f714ff60f00c198c4506ea1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-29 09:31:33 +0600

    add array last element by array dot length

--- array-literal.js
+++ array-literal.js
@@ -9,3 +9,17 @@ console.log(arr);
 
 // Length = LastIndex + 1
 // LastIndex = Length - 1
+
+const names = [
+  "Abul",
+  "Babul",
+  "Cabul",
+  "Dabul",
+  "Ebul",
+  "Jabul"
+]
+// names[5] = "Fabul";
+names[names.length] = "Fabul";
+
+console.log(names.length);
+console.log(names);
