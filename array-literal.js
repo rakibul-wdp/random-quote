d6ade23eb3db38eaad0f269afe5f586638f56bf9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-29 09:13:19 +0600

    lastIndex equal Length minus 1

--- array-literal.js
+++ array-literal.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// Array Literal
+const arr = [];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[99] = 100;
+console.log(arr.length);
+console.log(arr);
+
+// Length = LastIndex + 1
+// LastIndex = Length - 1
