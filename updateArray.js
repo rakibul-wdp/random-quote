35aa5c77631d908067fac2f860a3a877542f4f03
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-03 10:28:31 +0600

    create update array boilarplate

---
+++ updateArray.js
@@ -0,0 +1,18 @@
+/**
+ * @Title: Update Array Elements and Fill Array
+ */
+
+// Array Fill Manual Way
+const arr1 = new Array(10);
+console.log(arr1);
+
+// Array Fill
+const arr2 = new Array(10);
+
+const names = ["Abul", "Babul", "Cabul"];
+
+console.log(names);
+
+// Fill Array and Update
+
+// Array is mutable
