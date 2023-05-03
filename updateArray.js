e5bccfe09146056d8ceed2a1d7d2baee7fb52da0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-03 10:37:22 +0600

    array fill with manually and with fill

--- updateArray.js
+++ updateArray.js
@@ -4,13 +4,20 @@
 
 // Array Fill Manual Way
 const arr1 = new Array(10);
-console.log(arr1);
+for (let i = 0; i < arr1.length; i++) {
+  arr1[i] = false;
+}
+// console.log(arr1);
 
 // Array Fill
 const arr2 = new Array(10);
+arr2.fill(0);
+// console.log(arr2);
 
 const names = ["Abul", "Babul", "Cabul"];
-
+names[0] = "Cabul";
+names[1] = "Chandu";
+names[2] = "Abul";
 console.log(names);
 
 // Fill Array and Update
