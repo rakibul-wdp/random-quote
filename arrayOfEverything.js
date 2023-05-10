e3b38083421ed168f9854a2fe61e419421e92a98
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-10 15:31:02 +0600

    array of arryays

--- arrayOfEverything.js
+++ arrayOfEverything.js
@@ -24,11 +24,54 @@ const times = (a, b) => a * b;
 const div = (a, b) => a / b;
 const mod = (a, b) => a % b;
 
+// Loop and call all functions from array
 const funcs = [sum, sub, times, div, mod];
 const a = 10,
   b = 20;
 
 for (let i = 0; i < funcs.length; i++) {
   const result = funcs[i](a, b);
-  console.log(`[${funcs[i].name}] Result = ${result}`);
+  // console.log(`[${funcs[i].name}] Result = ${result}`);
 }
+
+// Array of Arrays - Multi Dimensional Array
+const pointTable = [
+  [0, 0],
+  [3, 5],
+  [5, 7],
+  [10, 23],
+];
+
+// One Dimensional Traverse
+for (let i = 0; i < pointTable.length; i++) {
+  // console.log(`Point ${i} - x = ${pointTable[i][0]} and y = ${pointTable[i][1]}`);
+}
+
+// Two Dimensional Traverse
+const numbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers[i].length; j++) {
+    // console.log(numbers[i][j]);
+  }
+}
+
+for (let i = 0; i < pointTable.length; i++) {
+  for (let j = 0; j < pointTable[i].length; j++) {
+    console.log(`Points [${i}, ${j}] = ${pointTable[i][j]}`);
+  }
+}
+
+/**
+ * [
+ * [1 2 3 4 5],
+ * [1 2 3 4 5],
+ * [1 2 3 4 5],
+ * [1 2 3 4 5],
+ * [1 2 3 4 5],
+ * ]
+ */
