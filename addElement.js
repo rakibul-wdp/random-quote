347d9180650d800c9d01c25ae5fe66e9cbdc9d2b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-12 14:30:50 +0600

    array splice

--- addElement.js
+++ addElement.js
@@ -19,4 +19,12 @@ arrU1.unshift(4);
 // arrU1.unshift(...arrU2);
 Array.prototype.unshift.apply(arrU1, arrU2);
 
-console.log(arrU1);
+// console.log(arrU1);
+
+// Use Splice to insert at the given index
+const arrS1 = [1, 2, 3, 5, 9];
+const arrS2 = [6, 7, 8];
+arrS1.splice(3, 0, 4);
+arrS1.splice(5, 0, ...arrS2);
+
+console.log(arrS1);
