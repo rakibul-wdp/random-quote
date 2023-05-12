fe8189c5fe2909293a9115c3c79eaa0f9c3b72dc
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-12 14:23:23 +0600

    array unshift

--- addElement.js
+++ addElement.js
@@ -10,5 +10,13 @@ arrP1.push(5);
 arrP1.push(6, 7);
 // arrP1.push(...arrP2);
 Array.prototype.push.apply(arrP1, arrP2);
-
 // console.log(arrP1);
+
+// Use unshift to insert at the beginning
+const arrU1 = [5, 6, 7];
+const arrU2 = [1, 2, 3];
+arrU1.unshift(4);
+// arrU1.unshift(...arrU2);
+Array.prototype.unshift.apply(arrU1, arrU2);
+
+console.log(arrU1);
