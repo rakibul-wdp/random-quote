7c10cdde5154e78231c07336efc8b3002b0e1064
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-16 08:45:10 +0600

    can run command

--- cliProject/src/index.js
+++ cliProject/src/index.js
@@ -3,3 +3,17 @@ const path = require("path");
 const Todo = require("./Todo");
 const { saveFile, readFile } = require("./utils");
 const { ADD, UPDATE, NEXT, DONE, FIND, LIST } = require("./commands");
+
+const fileName = "../data.json";
+const filePath = path.resolve(
+  __dirname,
+  fileName
+);
+
+(function init() {
+  const data = readFile(filePath) || [];
+  const todo = new Todo(data);
+  const { _: baseCommand } = argv;
+
+
+})();
